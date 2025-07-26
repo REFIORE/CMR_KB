1c7d1ce0457c553d60f96d65b72a6469cad91823
REFIORE <aras_andrey@bk.ru>
2025-07-26 16:51:47 +0300

    Правки

--- orders/signals.py
+++ orders/signals.py
@@ -1,15 +1,19 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CustomUser, Profile
+from .models import Profile
+from django.contrib.auth import get_user_model
 
 
-@receiver(post_save, sender=CustomUser)
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=CustomUser)
+@receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     if hasattr(instance, 'profile'):
         instance.profile.save()
